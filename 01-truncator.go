d08e6b4f87486cde867b1179e5d0801dd0586ac3
Ryan Christy <call900913@icloud.com>
2023-08-10 03:19:08 -0400

    Run go fmt on file 01

--- 01-truncator.go
+++ 01-truncator.go
@@ -5,9 +5,9 @@ import (
 	"strconv"
 )
 
-// program removes all the numbers to the right of the decimal point.
+// Program removes all the numbers to the right of the decimal point.
 func main() {
-	var entry float64 // declare the var to store userinput in.
+	var entry float64                              // declare the var to store userinput in.
 	fmt.Printf("Enter a float64: (e.g. 3.1415): ") // prompt user for input
 
 	_, e := fmt.Scan(&entry) // store it in where that pointer is pointing at.
@@ -16,8 +16,7 @@ func main() {
 		fmt.Println("Please enter a floating number, such as")
 	}
 
-	entry_truncated := strconv.FormatFloat(entry, 'f', 0, 64) // convert the float to string.
+	entry_truncated := strconv.FormatFloat(entry, 'f', 0, 64)                   // convert the float to string.
 	fmt.Printf("The number is now a string & truncated: %s\n", entry_truncated) // print the result
 
 }
-
