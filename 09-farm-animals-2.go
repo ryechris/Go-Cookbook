ee487e07c65da8a550c1830773f7b6decc6c3771
Riyan <riyan@linux.com>
2023-08-17 07:45:30 -0400

    Begin program 09

---
+++ 09-farm-animals-2.go
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 Riyan Christy. All rights reserved.
+
+package main
+
+import (
+    ""
+    ""
+)
+
+type Animal interface { // Use interface for its power
+    Eat()
+    Move()
+    Speak()
+}
+
+func main () {
+
+}
+
+// ToDo: the three functions Eat(), Move(), Speak
+// Database
