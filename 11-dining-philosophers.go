399c37780a9b814f21181f75944cd1e7d2f0852d
Riyan <riyan@linux.com>
2023-08-20 05:39:21 -0400

    Begin final file: Dining Philosophers

---
+++ 11-dining-philosophers.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2023 Riyan Christy. All rights reserved.
+// This program is an implementation of Dijkstra's Resource Hierarchy solution,
+// (After all, the Dining Philosophers was originally an exam problem 
+// he created for his students.)
+// Since this is Go, Riyan has modified a few things a little, 
+// but the essence is the same (as you can examine yourself).
+
+package
